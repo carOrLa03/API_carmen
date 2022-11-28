ea1423976a66699eeb3a527a8b7bd64e50a29026
CarOrLa03 <carmenorla03@gmail.com>
2022-11-28 21:09:50 +0100

    entendiendo

--- JS/validaRegistro.js
+++ JS/validaRegistro.js
@@ -1,28 +1,29 @@
-const enviado = document.getElementById('envia');
+const enviado = document.getElementById("envia");
 
-enviado.addEventListener('click', (e) => {
-    e.preventDefault()
-    let nombre = document.getElementById('nombre').value
-    let mail = document.getElementById('mail').value
-    let pass = document.getElementById('pass').value
-    let pass2 = document.getElementById('pass2').value
-    if (nombre == "" || mail == "" || pass == "" || pass2 == "") {
-        return alert('Todos los campos son obligatorios')
-    } else if (pass.length < 8 || pass2.length < 8) {
-        return alert('La contraseña debe tener más de 8 caracteres.')
-    } else if (pass != pass2) {
-        return alert('Tu password debe coincidir con la segunda contraseña')
-
-    } else {
-        return alert('Datos enviados correctamente.')
-        fetch('http://localhost:8000/api/user/', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ name: nombre, mail: mail, password: pass })
-        })
-            .then(res => res.json())
-            .then(res => { console.log(res); });
-    }
-})
+enviado.addEventListener("click", (e) => {
+  e.preventDefault();
+  let nombre = document.getElementById("nombre").value;
+  let mail = document.getElementById("mail").value;
+  let pass = document.getElementById("pass").value;
+  let pass2 = document.getElementById("pass2").value;
+  if (nombre == "" || mail == "" || pass == "" || pass2 == "") {
+    return alert("Todos los campos son obligatorios");
+  } else if (pass.length < 8 || pass2.length < 8) {
+    return alert("La contraseña debe tener más de 8 caracteres.");
+  } else if (pass != pass2) {
+    return alert("Tu password debe coincidir con la segunda contraseña");
+  } else {
+    return alert("Datos enviados correctamente.");
+    fetch("http://localhost:8000/api/user/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: nombre, mail: mail, password: pass }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  }
+});
